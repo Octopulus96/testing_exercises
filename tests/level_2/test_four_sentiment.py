13d75d0ede9c46007f1c02e17fc776a45ba19a20
Octopulus96 <octopulus96@gmail.com>
2024-04-13 17:58:02 +0300

    Добавил генерацию текста

--- tests/level_2/test_four_sentiment.py
+++ tests/level_2/test_four_sentiment.py
@@ -1,87 +1,62 @@
+import random
+from typing import Set, List
 import pytest
 from faker import Faker
 from functions.level_2.four_sentiment import check_tweet_sentiment
 
-# В теории faker сдесь не треюуется
-# Будет достаточно " ".join(list[str])
-# На практике подойдет фикстура генерирующая текст, а слова в списке являются уникальными в нем
+
+@pytest.fixture(name="random_text_with_unique_words")
+def fixture_random_text() -> str:
+    def factory(ext_word_set: Set[str]) -> str:
+        fake = Faker()
+        text = fake.text(200)
+        text_list: List[str] = text.split()
+        text_list = [word.lower() for word in text_list if word not in ext_word_set]
+        for word in ext_word_set:
+            random_index = random.randint(0, len(text_list))
+            text_list.insert(random_index, word)
+        modified_text = " ".join(text_list)
+        return modified_text
+
+    return factory
 
 
 @pytest.mark.parametrize(
-    "text, good_words, bad_words, expected",
+    "good_words, bad_words, expected",
     [
         pytest.param(
-            " ".join(
-                Faker().words(
-                    nb=6,
-                    ext_word_list=[
-                        "love",
-                        "happiness",
-                        "peace",
-                        "war",
-                        "pain",
-                        "death",
-                    ],
-                    unique=True,
-                )
-            ),
-            ("love", "happiness", "peace"),
-            ("war", "pain", "death"),
+            {"love", "happiness", "peace"},
+            {"war", "pain", "death"},
             None,
             id="bad_words_and_good_words_equally",
         ),
         pytest.param(
-            " ".join(
-                Faker().words(
-                    nb=5,
-                    ext_word_list=["love", "happiness", "peace", "war", "pain"],
-                    unique=True,
-                )
-            ),
-            ("love", "happiness", "peace"),
-            ("war", "pain", "death"),
+            {"love", "happiness", "peace"},
+            {"war", "pain"},
             "GOOD",
             id="more_good_words_than_bad",
         ),
         pytest.param(
-            " ".join(
-                Faker().words(
-                    nb=5,
-                    ext_word_list=["love", "happiness", "war", "pain", "death"],
-                    unique=True,
-                )
-            ),
-            ("love", "happiness", "peace"),
-            ("war", "pain", "death"),
+            {"love", "happiness"},
+            {"war", "pain", "death"},
             "BAD",
             id="more_bad_words_than_good",
-        ),
-        pytest.param(
-            " ".join(
-                Faker().words(
-                    nb=6,
-                    ext_word_list=(
-                        "love",
-                        "happiness",
-                        "peace",
-                        "war",
-                        "pain",
-                        "death",
-                    ),
-                    unique=True,
-                )
-            ),
-            [],
-            [],
-            None,
-            id="lists_of_good_and_bad_words_are_empty",
         ),
     ],
 )
 def test__check_tweet_sentiment__emotional_colouring_of_text(
-    text: str, good_words: set[str], bad_words: set[str], expected: str | None
+    random_text_with_unique_words: str, good_words: Set[str], bad_words: Set[str], expected: str | None
 ) -> None:
-    result = check_tweet_sentiment(
-        text=text, good_words=good_words, bad_words=bad_words
-    )
+    text = random_text_with_unique_words(good_words | bad_words)
+    result = check_tweet_sentiment(text=text, good_words=good_words, bad_words=bad_words)
     assert result == expected
+
+
+def test__check_tweet_sentiment__lists_of_good_and_bad_words_are_empty(
+    random_text_with_unique_words: str,
+) -> None:
+    text = random_text_with_unique_words(set())
+    result = check_tweet_sentiment(
+        text=text, good_words={"love", "happiness", "peace"}, bad_words={"war", "pain", "death"}
+    )
+    assert result is None
